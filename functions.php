ff6a3161a34458126e2e68403abe6720764ab0c8
nepamuceno <zzerver@gmail.com>
2020-08-09 15:58:32 -0500

    add bootstrap js and css~

--- functions.php
+++ functions.php
@@ -3,13 +3,18 @@
 /* attach theme scripts */
 function navarro_theme_load_scripts(){
 	// attach navarro.css
-	wp_enqueue_style("navarro.css", get_template_directory_uri (). "/assets/css/navarro.css", arrays, "1.0", "all");
-
+	//wp_enqueue_style("navarro.css", get_template_directory_uri (). "/assets/css/navarro.css", arrays, "1.0", "all");
+    wp_enqueue_style("bootstrap", get_template_directory_uri(). "/assets/vendor/bootstrap/css/bootstrap.min.css", arrays, "1.0", "all");
 	//attach style.css
-	wp_enqueue_style("theme-style", get_stylesheet_uri(), arrays, "1.0", "all");
+	//wp_enqueue_style("theme-style", get_stylesheet_uri(), arrays, "1.0", "all");
+	wp_enqueue_style("blog-home", get_template_directory_uri()."/assets/css/blog-home.css", arrays, "1.0", "all");
+	wp_enqueue_style("style", get_stylesheet_uri(), arrays, "1.0", "all");
 
 	//attach script.js and jquery
-	wp_enqueue_script("navarro-js", get_template_directory_uri()."/assets/js/navarro.js", array("jquery"), "1.0", true);
+	//wp_enqueue_script("navarro-js", get_template_directory_uri()."/assets/js/navarro.js", array("jquery"), "1.0", true);
+	//wp_enqueue_script("jquery-min", get_template_directory_uri()."/assets/vendor/jquery/jquery.min.js", array("jquery"), "1.0", true);
+	wp_enqueue_script("bootstrap", get_template_directory_uri()."/assets/vendor/bootstrap/js/bootstrap.bundle.min.js", array("jquery"), "1.0", true);
+
 }
 
 add_action("wp_enqueue_scripts", "navarro_theme_load_scripts");
